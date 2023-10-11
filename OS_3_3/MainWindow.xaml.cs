939e3962f4f72cb580339a485a9ac5599c3823c7
iNeutronn <57719073+iNeutronn@users.noreply.github.com>
2023-10-11 22:42:45 +0300

    added timer refreshment logic

--- OS_3_3/MainWindow.xaml.cs
+++ OS_3_3/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -21,11 +22,24 @@ namespace OS_3_3
     /// </summary>
     public partial class MainWindow : Window
     {
-        List<Process> processes = new(4);
+        const int TIMEOUT = 100;
+        private readonly List<Process> processes = new(4);
         public MainWindow()
         {
             InitializeComponent();
             ProcessInfGrid.ItemsSource = processes;
+            new Thread(new ThreadStart( () =>
+            {
+                while (true)
+                {
+                    if(!this.IsVisible) // if window is not visible, then stop updating
+                        Dispatcher.Invoke(() =>
+                        {
+                            ProcessInfGrid.Items.Refresh();
+                        });
+                    Thread.Sleep(TIMEOUT);
+                }
+            })).Start();
         }
 
         private void CreateProcessButton_Click(object sender, RoutedEventArgs e)
