31f3ca3dcee4e5c224bd994fb76869af81829a9c
moon-1945 <makozukviktor@gmail.com>
2023-10-11 18:11:34 +0300

    Add project files.

---
+++ OS_3_3/Process.cs
@@ -0,0 +1,330 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using static OS_3_3.WindowsApi;
+
+
+namespace OS_3_3
+{
+    public class Process
+    {
+        private string? _commandLine = null;
+        private IntPtr _handle = IntPtr.Zero;
+        //    private IntPtr _threadHandle; //???
+
+        public uint Id { get; private set; }
+
+        public string Name { get; private set; }
+
+        public Process(string commandLine)
+        {
+            _commandLine = commandLine;
+        }
+
+        public bool Start()
+        {
+            if (_commandLine == null)
+            {
+                throw new Exception("Process haven't info");
+            }
+
+            STARTUPINFO startupInfo = new STARTUPINFO();
+            PROCESS_INFORMATION processInfo = new PROCESS_INFORMATION();
+
+            startupInfo.cb = 104;
+
+            bool isSuccessful = CreateProcess(null,
+            _commandLine,
+            IntPtr.Zero,
+            IntPtr.Zero,
+            false,
+            0,
+            IntPtr.Zero,
+            null,
+            ref startupInfo,
+            out processInfo);
+
+            _handle = processInfo.hProcess;
+
+            Id = processInfo.dwProcessId;
+
+            CloseHandle(processInfo.hThread);
+
+            StringBuilder lpFileName = new StringBuilder((int)MAX_PATH); // 260 - MAX_PATH
+
+            K32GetModuleFileNameExW(_handle, IntPtr.Zero, lpFileName, MAX_PATH);
+
+            Name = Path.GetFileName(lpFileName.ToString());
+
+            return isSuccessful;
+        }
+
+        public static Process? Start(string commandLine)
+        {
+            var process = new Process(commandLine);
+
+            return process.Start() ? process : null;
+        }
+
+        public void WaitToEnd()
+        {
+            WaitForSingleObject(_handle, INFINITY);
+        }
+
+        public void Close()
+        {
+            WaitToEnd();
+            CloseHandle(_handle);
+        }
+
+        private uint[] GetThreadIDs()
+        {
+            List<uint> ids = new List<uint>();
+
+            IntPtr hThreadSnapshot = CreateToolhelp32Snapshot(TH32CS_SNAPTHREAD, 0);
+
+            if (hThreadSnapshot != IntPtr.Zero)
+            {
+                THREADENTRY32 threadEntry = new THREADENTRY32();
+                threadEntry.dwSize = (uint)Marshal.SizeOf(typeof(THREADENTRY32));
+
+                if (Thread32First(hThreadSnapshot, ref threadEntry))
+                {
+                    do
+                    {
+                        if (threadEntry.th32OwnerProcessID == Id)
+                        {
+                            ids.Add(threadEntry.th32ThreadID);
+                        }
+                    } while (Thread32Next(hThreadSnapshot, ref threadEntry));
+                }
+
+                CloseHandle(hThreadSnapshot);
+            }
+            else
+            {
+                Console.WriteLine("CreateToolhelp32Snapshot failed. Error code: " + Marshal.GetLastWin32Error());
+            }
+
+            return ids.ToArray();
+        }
+
+        public static uint[] GetProcessesIdByName(string name)
+        {
+            List<uint> ids = new List<uint>();
+
+            PROCESSENTRY32 entry = new PROCESSENTRY32();
+            entry.dwSize = (uint)Marshal.SizeOf(typeof(PROCESSENTRY32));
+
+            IntPtr snapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+
+            if (Process32First(snapshot, ref entry))
+            {
+                do
+                {
+                    if (Path.GetFileName(entry.szExeFile) == name || entry.szExeFile == name)
+                    {
+                        ids.Add(entry.th32ProcessID);
+                    }
+                } while (Process32Next(snapshot, ref entry));
+            }
+
+            CloseHandle(snapshot);
+
+            return ids.ToArray();
+        }
+
+
+        public static Process? GetProcessById(uint id)
+        {
+            IntPtr handle = OpenProcess(ProcessAccessFlags.ALL_ACCESS, false, id);
+
+            if (handle == IntPtr.Zero) return null;
+
+            StringBuilder lpFileName = new StringBuilder(260); // 260 - MAX_PATH
+
+            K32GetModuleFileNameExW(handle, IntPtr.Zero, lpFileName, 260);
+
+            string path = lpFileName.ToString();
+
+            Process? process = new Process(path)
+            {
+                Name = Path.GetFileName(path),
+                Id = id,
+            };
+
+            process._handle = handle;
+
+            return process;
+        }
+
+        public ulong GetAffinityMask()
+        {
+            if (!GetProcessAffinityMask(_handle, out UIntPtr processAffinityMask, out UIntPtr systemAffinityMask))
+            {
+                throw new Exception();
+            }
+
+            return processAffinityMask.ToUInt64();
+        }
+
+        public static ulong GetAffinityMask(uint id)
+        {
+            Process? process = GetProcessById(id);
+
+            if (process == null) throw new Exception();
+
+            return process.GetAffinityMask();
+        }
+
+        public void SetAffinityMask(ulong affinityMask)
+        {
+            if (!SetProcessAffinityMask(_handle, new UIntPtr(affinityMask)))
+            {
+                throw new Exception();
+            }
+        }
+
+        public static void SetAffinityMask(uint id, ulong affinityMask)
+        {
+            Process? process = GetProcessById(id);
+
+            if (process == null) throw new Exception();
+
+            process.SetAffinityMask(affinityMask);
+        }
+
+        public ProcessPriorityClass GetPriority()
+        {
+            return GetPriorityClass(_handle);
+        }
+
+        public static ProcessPriorityClass GetPriority(uint id)
+        {
+            Process? process = GetProcessById(id);
+
+            if (process == null) throw new Exception();
+
+            return process.GetPriority();
+        }
+
+        public void SetPriority(ProcessPriorityClass priorityClass)
+        {
+            if (!SetPriorityClass(_handle, priorityClass))
+            {
+                throw new Exception();
+            }
+        }
+
+        public static void SetPriority(uint id, ProcessPriorityClass priorityClass)
+        {
+            Process? process = GetProcessById(id);
+
+            if (process == null) throw new Exception();
+
+            process.SetPriority(priorityClass);
+        }
+
+        public void Suspend()
+        {
+            uint[] threadIds = GetThreadIDs();
+
+            for (int i = 0; i < threadIds.Length; i++)
+            {
+                IntPtr handle = OpenThread(ThreadAccessFlags.ALL_ACCESS, false, threadIds[i]);
+
+                if (handle != IntPtr.Zero)
+                {
+                    SuspendThread(handle);
+                }
+
+                CloseHandle(handle);
+            }
+        }
+
+        public static void Suspend(uint id)
+        {
+            Process? process = GetProcessById(id);
+
+            if (process == null) throw new Exception();
+
+            process.Suspend();
+        }
+
+        public static void Suspend(string name)
+        {
+            uint[] ids = GetProcessesIdByName(name);
+
+            for (int i = 0; i < ids.Length; i++)
+            {
+                Suspend(ids[i]);
+            }
+        }
+
+        public void Resume()
+        {
+            uint[] threadIds = GetThreadIDs();
+
+            for (int i = 0; i < threadIds.Length; i++)
+            {
+                IntPtr handle = OpenThread(ThreadAccessFlags.ALL_ACCESS, false, threadIds[i]);
+
+                if (handle != IntPtr.Zero)
+                {
+                    ResumeThread(handle);
+                }
+
+                CloseHandle(handle);
+            }
+        }
+
+        public static void Resume(uint id)
+        {
+            Process? process = GetProcessById(id);
+
+            if (process == null) throw new Exception();
+
+            process.Resume();
+        }
+
+        public static void Resume(string name)
+        {
+            uint[] ids = GetProcessesIdByName(name);
+
+            for (int i = 0; i < ids.Length; i++)
+            {
+                Resume(ids[i]);
+            }
+        }
+
+        public void Kill()
+        {
+            TerminateProcess(_handle, 9);
+            //close handle???
+        }
+
+        public static void Kill(uint id)
+        {
+            Process? process = GetProcessById(id);
+
+            if (process == null) throw new Exception();
+
+            process.Kill();
+        }
+
+        public static void Kill(string name)
+        {
+            uint[] ids = GetProcessesIdByName(name);
+
+            for (int i = 0; i < ids.Length; i++)
+            {
+                Kill(ids[i]);
+            }
+        }
+    }
+}
---
+++ OS_3_3/WindowsApi.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OS_3_3
+{
+    public static class WindowsApi
+    {
+
+        public const int TH32CS_SNAPTHREAD = 0x00000004;
+        public const uint THREAD_ALL_ACCESS = 0x001F03FF;
+        public const uint INFINITY = 0xFFFFFFFF;
+        public const uint MAX_PATH = 260;
+        public const uint TH32CS_SNAPPROCESS = 0x00000002;
+        public const int INVALID_HANDLE_VALUE = -1;
+
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool CreateProcess(
+               string lpApplicationName,
+               string lpCommandLine,
+               IntPtr lpProcessAttributes,
+               IntPtr lpThreadAttributes,
+               bool bInheritHandles,
+               uint dwCreationFlags,
+               IntPtr lpEnvironment,
+               string lpCurrentDirectory,
+               [In] ref STARTUPINFO lpStartupInfo,
+               out PROCESS_INFORMATION lpProcessInformation);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool CloseHandle(IntPtr hObject);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern uint WaitForSingleObject(IntPtr hHandle, uint dwMilliseconds);
+
+        [DllImport("kernel32.dll")]
+        public static extern uint GetLastError();
+
+        [DllImport("kernel32.dll")]
+        public static extern uint SuspendThread([In] IntPtr hThread);
+
+        [DllImport("kernel32.dll")]
+        public static extern uint ResumeThread([In] IntPtr hThread);
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Unicode)]
+        public static extern uint K32GetModuleFileNameExW(IntPtr hProcess, IntPtr hModule, [Out] StringBuilder lpBaseName, uint nSize);
+
+        [DllImport("kernel32.dll")]
+        public static extern IntPtr OpenProcess(ProcessAccessFlags dwDesiredAccess, bool bInheritHandle, uint dwProcessId);
+
+        [DllImport("kernel32.dll")]
+        public static extern IntPtr CreateToolhelp32Snapshot(uint dwFlags, uint th32ProcessID);
+
+        [DllImport("kernel32.dll")]
+        public static extern bool Thread32First(IntPtr hSnapshot, ref THREADENTRY32 lpte);
+
+        [DllImport("kernel32.dll")]
+        public static extern bool Thread32Next(IntPtr hSnapshot, ref THREADENTRY32 lpte);
+
+        [DllImport("kernel32.dll")]
+        public static extern bool Process32First(IntPtr hSnapshot, ref PROCESSENTRY32 lppe);
+
+        [DllImport("kernel32.dll")]
+        public static extern bool Process32Next(IntPtr hSnapshot, ref PROCESSENTRY32 lppe);
+
+        [DllImport("kernel32.dll")]
+        public static extern IntPtr OpenThread(ThreadAccessFlags dwDesiredAccess, bool bInheritHandle, uint dwThreadId);
+
+        [DllImport("kernel32.dll")]
+        public static extern bool GetProcessAffinityMask(IntPtr hProcess, out UIntPtr processAffinityMask, out UIntPtr systemAffinityMask);
+
+        [DllImport("kernel32.dll")]
+        public static extern bool SetProcessAffinityMask(IntPtr hProcess, UIntPtr processAffinityMask);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern ProcessPriorityClass GetPriorityClass(IntPtr hProcess);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern bool SetPriorityClass(IntPtr hProcess, ProcessPriorityClass dwPriorityClass);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern bool TerminateProcess(IntPtr hProcess, uint uExitCode);
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct STARTUPINFO
+        {
+            public int cb;
+            public string lpReserved;
+            public string lpDesktop;
+            public string lpTitle;
+            public uint dwX;
+            public uint dwY;
+            public uint dwXSize;
+            public uint dwYSize;
+            public uint dwXCountChars;
+            public uint dwYCountChars;
+            public uint dwFillAttribute;
+            public uint dwFlags;
+            public short wShowWindow;
+            public short cbReserved2;
+            public IntPtr lpReserved2;
+            public IntPtr hStdInput;
+            public IntPtr hStdOutput;
+            public IntPtr hStdError;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct PROCESS_INFORMATION
+        {
+            public IntPtr hProcess;
+            public IntPtr hThread;
+            public uint dwProcessId;
+            public uint dwThreadId;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct THREADENTRY32
+        {
+            public uint dwSize;
+            public uint cntUsage;
+            public uint th32ThreadID;
+            public uint th32OwnerProcessID;
+            public uint tpBasePri;
+            public uint tpDeltaPri;
+            public uint dwFlags;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct PROCESSENTRY32
+        {
+            public uint dwSize;
+            public uint cntUsage;
+            public uint th32ProcessID;
+            public IntPtr th32DefaultHeapID;
+            public uint th32ModuleID;
+            public uint cntThreads;
+            public uint th32ParentProcessID;
+            public int pcPriClassBase;
+            public uint dwFlags;
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 260)]
+            public string szExeFile;
+        }
+
+    }
+}
+
+[Flags]
+public enum ProcessAccessFlags : uint
+{
+    TERMINATE = 0x0001,
+    CREATE_THREAD = 0x0002,
+    SET_SESSIONID = 0x0004,
+    VM_OPERATION = 0x0008,
+    VM_READ = 0x0010,
+    VM_WRITE = 0x0020,
+    DUP_HANDLE = 0x0040,
+    CREATE_PROCESS = 0x0080,
+    SET_QUOTA = 0x0100,
+    SET_INFORMATION = 0x0200,
+    QUERY_INFORMATION = 0x0400,
+    SUSPEND_RESUME = 0x0800,
+    QUERY_LIMITED_INFORMATION = 0x1000,
+    SET_LIMITED_INFORMATION = 0x2000,
+    ALL_ACCESS = 0xFFFF,
+}
+
+[Flags]
+public enum ThreadAccessFlags : uint
+{
+    TERMINATE = 0x0001,
+    SUSPEND_RESUME = 0x0002,
+    GET_CONTEXT = 0x0008,
+    SET_CONTEXT = 0x0010,
+    QUERY_INFORMATION = 0x0040,
+    SET_INFORMATION = 0x0020,
+    SET_THREAD_TOKEN = 0x0080,
+    IMPERSONATE = 0x0100,
+    DIRECT_IMPERSONATION = 0x0200,
+    SET_LIMITED_INFORMATION = 0x0400,
+    QUERY_LIMITED_INFORMATION = 0x0800,
+    RESUME = 0x1000,
+    ALL_ACCESS = 0xFFFF,
+}
+
+
+public enum ProcessPriorityClass : uint
+{
+    ABOVE_NORMAL = 0x8000,
+    BELOW_NORMAL = 0x4000,
+    HIGH = 0x80,
+    IDLE = 0x40,
+    NORMAL = 0x20,
+    REALTIME = 0x100
+}
+
+
+
+
+
